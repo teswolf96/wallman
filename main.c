8f548a8b0a8f0c4692e873129aadec3e7e5c64a9
Lucifer <luciferms616@gmail.com>
2018-01-18 13:47:40 -0600

    Meow

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 #include <ctype.h>
 #include "config_parse.h"
 
-#define NUM_PROFILES 200 //Does anyone really need more than 200 profiles?
+//#define NUM_PROFILES 200 //Does anyone really need more than 200 profiles?
 
 int load_profiles();
 
 int list_profiles();
 
-int save_profiles();
+//int save_profiles();
 
 //int set_profile(char *profile_name);
 
@@ -35,7 +35,7 @@ char* int_to_bool(int bool);
 int wallpapers_equal(struct wallpaper wall1, struct wallpaper wall2);
 
 struct Config config;
-struct wallpaper profiles[NUM_PROFILES];
+//struct wallpaper profiles[NUM_PROFILES];
 char curr_wallpaper[256];
 int num_profiles;
 
@@ -55,7 +55,7 @@ int main(int argc, char **argv) {
         printf(" profile_name - set profile\n");
         printf(" -d --display profile_name display_name - set a profile's display name\n");
         printf(" -C --category category_name profile_name - set a profile's category (not required)\n");
-        printf(" -D --duplicate profile_name new_profile_name - Create a copy of a profile with a new name\n");
+        //printf(" -D --duplicate profile_name new_profile_name - Create a copy of a profile with a new name\n");
         printf("\n");
         printf("Examples:\n");
         printf(" wallman cats    Apply the cats profile\n");
@@ -82,27 +82,14 @@ int main(int argc, char **argv) {
     }
 
 
-    num_profiles = 0;
-    for (int idx = 0; idx < NUM_PROFILES; idx++) {
-        strncpy(profiles[idx].name, "undefined name", 256);
-
-    }
-
-    //int load_status = load_profiles();
-//    printf("Calling new load profile:\n");
-    int load_status = 0;
-    config = load_profiles_new();
-
-//    printf("Active Profile: %s\n",config.active_profile);
-//    printf("Current Wallpaper:\n");
-//    print_wallpaper(config.current);
-//    printf("Wallpapers In Profile:\n");
-//    for(int idx=0;idx<vector_size(config.wallpaper_list);idx++){
-//        print_wallpaper(config.wallpaper_list[idx]);
+//    num_profiles = 0;
+//    for (int idx = 0; idx < NUM_PROFILES; idx++) {
+//        strncpy(profiles[idx].name, "undefined name", 256);
+//
 //    }
 
-
-
+    int load_status = 0;
+    config = load_profiles_new();
 
     if (strncmp(argv[1], "--list", 256) == 0 ||
         strncmp(argv[1], "-l", 256) == 0) {
@@ -121,8 +108,8 @@ int main(int argc, char **argv) {
         strncpy(profile_name, argv[3], 256);
 
         int profile_num = get_wallpaper_num(profile_name);
-        strncpy(profiles[profile_num].category, cat_name, 256);
-        save_profiles();
+        strncpy(config.wallpaper_list[profile_num].category, cat_name, 256);
+        //save_profiles();
 
         return 0;
 
@@ -149,7 +136,7 @@ int main(int argc, char **argv) {
         } else if (argc == 5) {
             int mon_num = atoi(argv[3]);
             set_path(argv[2], mon_num, argv[4]);
-            save_profiles();
+            //save_profiles();
         }
         return 0;
     } else if (strncmp(argv[1], "--current", 256) == 0 ||
@@ -164,29 +151,7 @@ int main(int argc, char **argv) {
         }
         set_profile_disp_name(argv[2], argv[3]);
         return 0;
-    } else if (strncmp(argv[1], "--duplicate", 256) == 0 ||
-               strncmp(argv[1], "-D", 256) == 0) {
-        if (argc < 4) {
-            printf("Not enough arguments\n");
-            return 0;
-        }
 
-        int profile_num = get_wallpaper_num(argv[2]);
-
-        strncpy(profiles[num_profiles].name,argv[3],256);
-        profiles[num_profiles].mon_num = profiles[profile_num].mon_num;
-        strncpy(profiles[num_profiles].disp_name,argv[3],256);
-        strncpy(profiles[num_profiles].category,profiles[profile_num].category,256);
-        for(int idx=0;idx<profiles[num_profiles].mon_num;idx++){
-            strncpy(profiles[num_profiles].paths[idx],profiles[profile_num].paths[idx],256);
-        }
-        num_profiles++;
-
-        save_profiles();
-
-        printf("Duplicated %s to %s\n",argv[2],argv[3]);
-
-        return 0;
 
     } else if (strncmp(argv[1], "--remove", 256) == 0 ||
                strncmp(argv[1], "-r", 256) == 0) {
@@ -244,7 +209,7 @@ int get_wallpaper_num(char *profile_name) {
 
     for (int idx = 0; idx < num_profiles; idx++) {
         //printf("Comparing %s to %s",argv[2],profiles[idx].name);
-        if (strncmp(profile_name, profiles[idx].name, 256) == 0) {
+        if (strncmp(profile_name, config.wallpaper_list[idx].name, 256) == 0) {
             conv = idx;
             break;
         }
@@ -289,7 +254,7 @@ int set_profile_disp_name(char *profile_name, char *disp_name) {
         //printf("Word detected\n");
         for (int idx = 0; idx < num_profiles; idx++) {
             //printf("Comparing %s to %s",argv[2],profiles[idx].name);
-            if (strncmp(profile_name, profiles[idx].name, 256) == 0) {
+            if (strncmp(profile_name, config.wallpaper_list[idx].name, 256) == 0) {
                 conv = idx;
                 break;
             }
@@ -304,9 +269,9 @@ int set_profile_disp_name(char *profile_name, char *disp_name) {
 
     //printf("Attempting to change: %s display name to %s\n",profiles[conv].name, disp_name);
 
-    strncpy(profiles[conv].disp_name, disp_name, 256);
+    strncpy(config.wallpaper_list[conv].disp_name, disp_name, 256);
 
-    save_profiles();
+    //save_profiles();
 
 }
 
@@ -356,71 +321,71 @@ int list_profiles() {
 }
 
 //Come back to this later
-int save_profiles() {
-    //printf("Saving profiles\n");
-    //char *fileName = strcat(getenv("HOME"), "/.config/wallman");
-    char *fileName = getenv("HOME");
-    //printf("hi! %s - :-)\n",fileName);
-    FILE *config = fopen(fileName, "w+"); /* should check the result */
-
-    if (config == NULL) {
-        fileName = "/etc/wallman";
-        config = fopen(fileName, "r");
-        if (config == NULL) {
-            printf("File could not be opened\n");
-            return -1;
-        }
-    }
-    //fprintf(config,"meow!\n");
-    //fputs("meow2!\n",config);
-    char current[100] = "";
-    sprintf(current, "current: %s\n", curr_wallpaper);
-    fprintf(config, current);
-
-    int profile_idx = 0;
-    while (profile_idx < NUM_PROFILES && strncmp(profiles[profile_idx].name, "undefined name", 256) != 0) {
-        char name_bak[50];
-        strncpy(name_bak, profiles[profile_idx].name, 256);
-        char *title = strcat(profiles[profile_idx].name, ":");
-        if (strncmp(profiles[profile_idx].disp_name, name_bak, 256) == 0) {
-            /* They are the same */
-            title = strcat(name_bak, ":\n");
-        } else {
-            //printf("%s != %s\n",profiles[profile_idx].disp_name,profiles[profile_idx].name);
-            title = strcat(title, " ");
-            title = strcat(title, profiles[profile_idx].disp_name);
-            title = strcat(title, "\n");
-        }
-        fprintf(config, title);
-        char monitor_count_str[10] = "";
-        sprintf(monitor_count_str, "    monitors: %d\n", profiles[profile_idx].mon_num);
-        fprintf(config, monitor_count_str);
-
-        char cat_name[256] = "";
-        sprintf(cat_name, "    category: %s\n", profiles[profile_idx].category);
-        fprintf(config, cat_name);
-
-        for (int idx = 0; idx < profiles[profile_idx].mon_num; idx++) {
-            char path[500] = "";
-            sprintf(path, "    %s\n", profiles[profile_idx].paths[idx]);
-            fprintf(config, path);
-        }
-        fprintf(config, "\n");
-        profile_idx++;
-    }
-
-
-    fclose(config);
-
-    char command[1024] = "export HOME=/home/"; //lucifer && exec wallman-genmenu";
-    char *user = getenv("USER");
-    strcat(command, user);
-    strcat(command, " && exec wallman-genmenu;");
-    strcat(command, " > /dev/null 2>&1"); /* Hide any errors because it looks better */
-    //printf("Command to apply: %s\n", command);
-    system(command);
-
-}
+//int save_profiles() {
+//    //printf("Saving profiles\n");
+//    //char *fileName = strcat(getenv("HOME"), "/.config/wallman");
+//    char *fileName = getenv("HOME");
+//    //printf("hi! %s - :-)\n",fileName);
+//    FILE *config = fopen(fileName, "w+"); /* should check the result */
+//
+//    if (config == NULL) {
+//        fileName = "/etc/wallman";
+//        config = fopen(fileName, "r");
+//        if (config == NULL) {
+//            printf("File could not be opened\n");
+//            return -1;
+//        }
+//    }
+//    //fprintf(config,"meow!\n");
+//    //fputs("meow2!\n",config);
+//    char current[100] = "";
+//    sprintf(current, "current: %s\n", curr_wallpaper);
+//    fprintf(config, current);
+//
+//    int profile_idx = 0;
+//    while (profile_idx < NUM_PROFILES && strncmp(profiles[profile_idx].name, "undefined name", 256) != 0) {
+//        char name_bak[50];
+//        strncpy(name_bak, profiles[profile_idx].name, 256);
+//        char *title = strcat(profiles[profile_idx].name, ":");
+//        if (strncmp(profiles[profile_idx].disp_name, name_bak, 256) == 0) {
+//            /* They are the same */
+//            title = strcat(name_bak, ":\n");
+//        } else {
+//            //printf("%s != %s\n",profiles[profile_idx].disp_name,profiles[profile_idx].name);
+//            title = strcat(title, " ");
+//            title = strcat(title, profiles[profile_idx].disp_name);
+//            title = strcat(title, "\n");
+//        }
+//        fprintf(config, title);
+//        char monitor_count_str[10] = "";
+//        sprintf(monitor_count_str, "    monitors: %d\n", profiles[profile_idx].mon_num);
+//        fprintf(config, monitor_count_str);
+//
+//        char cat_name[256] = "";
+//        sprintf(cat_name, "    category: %s\n", profiles[profile_idx].category);
+//        fprintf(config, cat_name);
+//
+//        for (int idx = 0; idx < profiles[profile_idx].mon_num; idx++) {
+//            char path[500] = "";
+//            sprintf(path, "    %s\n", profiles[profile_idx].paths[idx]);
+//            fprintf(config, path);
+//        }
+//        fprintf(config, "\n");
+//        profile_idx++;
+//    }
+//
+//
+//    fclose(config);
+//
+//    char command[1024] = "export HOME=/home/"; //lucifer && exec wallman-genmenu";
+//    char *user = getenv("USER");
+//    strcat(command, user);
+//    strcat(command, " && exec wallman-genmenu;");
+//    strcat(command, " > /dev/null 2>&1"); /* Hide any errors because it looks better */
+//    //printf("Command to apply: %s\n", command);
+//    system(command);
+//
+//}
 
 int set_path(char profile_name[80], int mon_num, char path[160]) {
     printf("Setting profile: %s on monitor %d to path %s\n", profile_name, mon_num, path);
